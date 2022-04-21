c5e12e060ef92cbc294580e6fb8258381fb53682
Bernardo Campos <62954018+bernardocamps@users.noreply.github.com>
2022-04-21 16:19:36 -0300

    creating the main menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MainClass 
+{
+    private static void Task1()
+    {
+        Console.Clear();
+        Console.WriteLine("Running Task 1.");
+        Console.ReadLine();
+    }
+
+    private static void Task2()
+    {
+        Console.Clear();
+        Console.WriteLine("Running Task 2.");
+        Console.ReadLine();
+    }
+
+    private static bool MainMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("Choose an option:");
+        Console.WriteLine("1. Run Task 1.");
+        Console.WriteLine("2. Run Task 2.");
+        Console.WriteLine("3. Exit");
+        Console.Write("\r\nOption: ");
+
+        switch (Console.ReadLine())     
+        {
+            case "1":
+                Task1();
+                return true;
+            case "2":
+                Task2();
+                return true;
+            case "3":
+                return false;    
+            default:
+                return true;
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        while (MainMenu());
+    }
+}
+
