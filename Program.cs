24c4aee72e79226ba8fdb8a9ceca972fc72d2f80
Bernardo Campos <62954018+bernardocamps@users.noreply.github.com>
2022-05-04 20:25:57 -0300

    Change menu options to register and sell product

--- Program.cs
+++ Program.cs
@@ -1,41 +1,24 @@
 ﻿class MainClass 
 {
-    private static void Banana()
-    {
-        Console.Clear();
-        Product product = new Product();
-        product.description = "Banana";
-        product.price = 1.99F;
-        product.quantity = 0;
-        Console.WriteLine("The {0} costs ${1} and {2}.", product.description, product.price, product.inStock());
-        Console.ReadLine();
-    }
-
-    private static void Apple()
-    {
-        Console.Clear();
-        Product product = new Product("Apple", 2.99F, 2);
-        Console.WriteLine("The {0} costs ${1} and {2}.", product.description, product.price, product.inStock());
-        Console.ReadLine();
-    }
-
     private static bool MainMenu()
     {
         Console.Clear();
         Console.WriteLine("Welcome!");
         Console.WriteLine("Choose an option:");
-        Console.WriteLine("1. Banana");
-        Console.WriteLine("2. Apple");
+        Console.WriteLine("1. Register product");
+        Console.WriteLine("2. Sell product");
         Console.WriteLine("3. Exit");
         Console.Write("\r\nOption: ");
 
         switch (Console.ReadLine())
         {
             case "1":
-                Banana();
+                Console.WriteLine("Product registered");
+                Console.ReadKey();
                 return true;
             case "2":
-                Apple();
+                Console.WriteLine("Product sold");
+                Console.ReadKey();
                 return true;
             case "3":
                 return false;    
@@ -49,4 +32,3 @@
         while (MainMenu());
     }
 }
-
